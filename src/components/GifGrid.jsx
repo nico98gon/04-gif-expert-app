b40eea58cbc51d39edcef31ef8ea58d9286889e0
Nicolas Gonzalez <nicoaugustogonzalez14@gmail.com>
2022-07-14 10:48:16 -0300

    82. useEffect

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,8 +1,12 @@
+import { useEffect } from "react";
 import { getGifs } from "../helpers/getGifs";
 
 export const GifGrid = ({category}) => {
     
-    getGifs(category);
+    useEffect( () => {
+        getGifs(category);
+    }, []);
+
 
     return (
         <>
